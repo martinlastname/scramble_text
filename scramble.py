b98cf386e389b294afc0159899b361d0c92944fd
Martin Matasovic <martin@martinm.io>
2016-09-08 00:15:50 -0700

    Initial commit

---
+++ scramble.py
@@ -0,0 +1,20 @@
+
+
+# Takes a list of words and returns a new list of scrambled words.
+def scrambleList(words):
+
+
+
+# Takes a word and returns that word scrambled.
+def scrambleWord(word):
+
+
+
+
+
+def main:
+	print(scrambleWord("Jello")
+
+
+
+main()
